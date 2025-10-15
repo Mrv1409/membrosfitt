8624abb03404e7f1ba550797a1152a367f15346e
Marvin Costa <marvincosta321@gmail.com>
2025-10-14 23:24:49 -0300

    altera arquivo firebase para corrigir chaves

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+  apiKey: "AIzaSyDjVR74bc5RSv5mTAMNAaNIGH_YqNaLzAY", 
+  authDomain: "membrosfit.firebaseapp.com",
+  projectId: "membrosfit", 
+  storageBucket: "membrosfit.firebasestorage.app",
+  messagingSenderId: "312256697645", 
+  appId: "1:312256697645:web:587202fbb3fd75e1c2efa4", 
+  measurementId: "G-C2179VJZW0" 
 };
 
 export const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
